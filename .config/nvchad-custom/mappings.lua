8a81596cfbe92c9347cf798d261ead4d89351fa1
Stepan Ponomarev <stponomarevw@gmail.com>
2023-11-14 20:03:47 +0300

    upd: update nvim config

--- .config/nvchad-custom/mappings.lua
+++ .config/nvchad-custom/mappings.lua
@@ -17,6 +17,37 @@ M.general = {
 	},
 }
 
--- more keybinds!
+M.disabled = {
+  n = {
+      ["<tab>"] = "",
+      ["<S-tab>"] = ""
+  }
+}
+
+M.tabufline = {
+  n = {
+    -- cycle through buffers
+    ["<leader>k"] = {
+      function()
+        require("nvchad.tabufline").tabuflineNext()
+      end,
+      "Goto next buffer",
+    },
+
+    ["<leader>j"] = {
+      function()
+        require("nvchad.tabufline").tabuflinePrev()
+      end,
+      "Goto prev buffer",
+    },
+  },
+}
+
+M.telescope = {
+    n = {
+        ["<leader>gr"] = { "<cmd> Telescope lsp_references <CR>", "Open references"},
+        ["<leader>fd"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Search through symbols"},
+    },
+}
 
 return M
