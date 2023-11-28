abf58fc90bebfeaece6185e9b8982b2b8ac8aa3c
Stepan Ponomarev <stponomarevw@gmail.com>
2023-11-29 02:23:26 +0300

    feat: add leetcode.nvim

--- .config/nvchad-custom/plugins.lua
+++ .config/nvchad-custom/plugins.lua
@@ -61,6 +61,26 @@ local plugins = {
         lazy = false,
     },
 
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
+
+            -- optional
+            "nvim-treesitter/nvim-treesitter",
+            "rcarriga/nvim-notify",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            -- configuration goes here
+        },
+        lazy = false,
+    },
+
+
 	-- To make a plugin not be loaded
 	-- {
 	--   "NvChad/nvim-colorizer.lua",
