5b9677f088db8cdbbd14a2b65aa7ce8411e76b97
Stepan Ponomarev <stponomarevw@gmail.com>
2024-05-13 11:07:52 +0300

    nvim: change gitsigns colors

---
+++ .config/nvim/lua/sap/colors.lua
@@ -0,0 +1,6 @@
+local colors = require 'ayu.colors'
+colors.generate() -- Pass `true` to enable mirage
+vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { fg = colors.comment })
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = colors.vcs_added })
+vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = colors.vcs_modified })
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = colors.vcs_removed })
