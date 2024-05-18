b377459d451da1cfa2264cd98a938cdcc14a28de
Stepan Ponomarev <stponomarevw@gmail.com>
2024-05-18 18:46:53 +0300

    nvim(image): render all images on buffer

--- .config/nvim/lua/plugins/images.lua
+++ .config/nvim/lua/plugins/images.lua
@@ -13,7 +13,7 @@ return {
           enabled = true,
           clear_in_insert_mode = false,
           download_remote_images = true,
-          only_render_image_at_cursor = true,
+          only_render_image_at_cursor = false,
           filetypes = { 'markdown', 'quarto' }, -- markdown extensions (ie. quarto) can go here
         },
         -- neorg = {
