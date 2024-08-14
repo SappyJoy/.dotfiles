fe4c62abb3fbd9af0e092ce70e40509dddab5ad6
Stepan Ponomarev <stponomarevw@gmail.com>
2024-08-15 02:18:46 +0300

    nvim(ai): add ChatGPT

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -25,4 +25,28 @@ return {
       require('codeium').setup {}
     end,
   },
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('chatgpt').setup {
+        api_key_cmd = 'pass show api/tokens/openai',
+        openai_params = {
+          model = 'gpt-4-turbo',
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 4095,
+          temperature = 0.2,
+          top_p = 0.1,
+          n = 1,
+        },
+      }
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
 }
