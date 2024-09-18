97f8d5886efdcccc17ac1456d518455649a59d18
Stepan Ponomarev <stponomarevw@gmail.com>
2024-09-18 15:16:25 +0300

    nvim(git): diffview open in visual mode

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -66,8 +66,11 @@ return {
         {
           { '<leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Diffview Close' },
           { '<leader>df', '<cmd>DiffviewFileHistory<cr>', desc = 'Diffview Open File History' },
-          { '<leader>do', '<cmd>DiffviewOpen<cr>', desc = 'Diffview Open' },
         },
+        {
+          mode = { 'n', 'v' },
+          { '<leader>do', '<cmd>DiffviewOpen<cr>', desc = 'Diffview Open' },
+        }
       }
     end,
   },
@@ -105,40 +108,96 @@ return {
         mode = 'v',
       },
     },
-  --   opts = {
-  --     mappings = {
-  --       popup = {
-  --         ['F'] = 'PullPopup',
-  --         ['p'] = false,
-  --       },
-  --       rebase_editor = {
-  --         ['<c-d>'] = 'Abort',
-  --         ['<c-c><c-k>'] = false,
-  --       },
-  --       commit_editor = {
-  --         ['<c-d>'] = 'Abort',
-  --         ['<c-c><c-k>'] = false,
-  --       },
-  --     },
-  --     console_timeout = 3000,
-  --     telescope_sorter = function()
-  --       return require('telescope').extensions.fzf.native_fzf_sorter()
-  --     end,
-  --     fetch_after_checkout = true,
-  --     auto_show_console = true,
-  --     disable_hint = true,
-  --     notification_icon = ' ',
-  --     status = {
-  --       show_head_commit_hash = false,
-  --     },
-  --     sections = {
-  --       rebase = {
-  --         folded = false,
-  --       },
-  --       recent = {
-  --         folded = false,
-  --       },
-  --     },
-  --   },
+    -- config = function()
+    --   require('neogit').setup {
+    --     auto_refresh = false,
+    --     console_timeout = 10000,
+    --     disable_context_highlighting = true,
+    --     disable_commit_confirmation = false,
+    --     disable_builtin_notifications = true,
+    --     disable_insert_on_commit = false,
+    --     signs = {
+    --       -- { CLOSED, OPENED }
+    --       section = { '', '' },
+    --       item = { '', '' },
+    --       hunk = { '', '' },
+    --     },
+    --     commit_editor = {
+    --       kind = 'vsplit',
+    --       show_staged_diff = false,
+    --     },
+    --   }
+    --
+    --   -- require('helper').nnoremap('<Leader>gg', '<CMD>Neogit<CR>')
+    --   local Color = require('neogit.lib.color').Color
+    --   local colors = require 'ayu.colors'
+    --   colors.generate(false) -- Pass `true` to enable mirage
+    --
+    --   local base_green = Color.from_hex '#99BF4D'
+    --   local base_red = Color.from_hex '#F27983'
+    --
+    --   local green = base_green:to_css()
+    --   local bg_green = base_green:shade(0):to_css()
+    --   local line_green = base_green:shade(0):set_saturation(0.2):to_css()
+    --
+    --   local red = base_red:to_css()
+    --   local bg_red = base_red:shade(0):to_css()
+    --   local line_red = base_red:shade(0):set_saturation(0.4):to_css()
+    --
+    --   vim.api.nvim_set_hl(0, 'NeogitDiffAdd', {
+    --     bg = line_green,
+    --     fg = bg_green,
+    --   })
+    --
+    --   vim.api.nvim_set_hl(0, 'NeogitDiffDelete', {
+    --     bg = line_red,
+    --     fg = bg_red,
+    --   })
+    --
+    --   vim.api.nvim_set_hl(0, 'NeogitDiffAddHighlight', {
+    --     bg = line_green,
+    --     fg = green,
+    --   })
+    --
+    --   vim.api.nvim_set_hl(0, 'NeogitDiffDeleteHighlight', {
+    --     bg = line_red,
+    --     fg = red,
+    --   })
+    -- end,
+    --   opts = {
+    --     mappings = {
+    --       popup = {
+    --         ['F'] = 'PullPopup',
+    --         ['p'] = false,
+    --       },
+    --       rebase_editor = {
+    --         ['<c-d>'] = 'Abort',
+    --         ['<c-c><c-k>'] = false,
+    --       },
+    --       commit_editor = {
+    --         ['<c-d>'] = 'Abort',
+    --         ['<c-c><c-k>'] = false,
+    --       },
+    --     },
+    --     console_timeout = 3000,
+    --     telescope_sorter = function()
+    --       return require('telescope').extensions.fzf.native_fzf_sorter()
+    --     end,
+    --     fetch_after_checkout = true,
+    --     auto_show_console = true,
+    --     disable_hint = true,
+    --     notification_icon = ' ',
+    --     status = {
+    --       show_head_commit_hash = false,
+    --     },
+    --     sections = {
+    --       rebase = {
+    --         folded = false,
+    --       },
+    --       recent = {
+    --         folded = false,
+    --       },
+    --     },
+    --   },
   },
 }
