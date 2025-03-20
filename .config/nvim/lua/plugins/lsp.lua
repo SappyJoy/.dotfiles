50e697ef0eb45b277db29abb68944b02f4185048
Stepan Ponomarev <stponomarevw@gmail.com>
2025-03-20 15:05:18 +0300

    nvim(lsp): disabled ltex - too mach RAM and I don't really use it

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -195,46 +195,46 @@ return {
         },
         xmlformatter = {},
         -- sqls = {},
-        ltex = {
-          use_spellfile = false,
-          filetypes = { 'latex', 'tex', 'bib', 'markdown', 'gitcommit', 'text' },
-          flags = { debounce_text_changes = 300 },
-          settings = {
-            ltex = {
-              enabled = { 'latex', 'tex', 'bib', 'markdown' },
-              language = 'ru',
-              diagnosticSeverity = 'information',
-              sentenceCacheSize = 2000,
-              additionalRules = {
-                enablePickyRules = true,
-                motherTongue = 'ru',
-              },
-              dictionary = (function()
-                -- For dictionary, search for files in the runtime to have
-                -- and include them as externals the format for them is
-                -- dict/{LANG}.txt
-                --
-                -- Also add dict/default.txt to all of them
-                local files = {}
-                for _, file in ipairs(vim.api.nvim_get_runtime_file('dict/*', true)) do
-                  local lang = vim.fn.fnamemodify(file, ':t:r')
-                  local fullpath = vim.fs.normalize(file, ':p')
-                  files[lang] = { ':' .. fullpath }
-                end
-
-                if files.default then
-                  for lang, _ in pairs(files) do
-                    if lang ~= 'default' then
-                      vim.list_extend(files[lang], files.default)
-                    end
-                  end
-                  files.default = nil
-                end
-                return files
-              end)(),
-            },
-          },
-        },
+        -- ltex = {
+        --   use_spellfile = false,
+        --   filetypes = { 'latex', 'tex', 'bib', 'markdown', 'gitcommit', 'text' },
+        --   flags = { debounce_text_changes = 300 },
+        --   settings = {
+        --     ltex = {
+        --       enabled = { 'latex', 'tex', 'bib', 'markdown' },
+        --       language = 'ru',
+        --       diagnosticSeverity = 'information',
+        --       sentenceCacheSize = 2000,
+        --       additionalRules = {
+        --         enablePickyRules = true,
+        --         motherTongue = 'ru',
+        --       },
+        --       dictionary = (function()
+        --         -- For dictionary, search for files in the runtime to have
+        --         -- and include them as externals the format for them is
+        --         -- dict/{LANG}.txt
+        --         --
+        --         -- Also add dict/default.txt to all of them
+        --         local files = {}
+        --         for _, file in ipairs(vim.api.nvim_get_runtime_file('dict/*', true)) do
+        --           local lang = vim.fn.fnamemodify(file, ':t:r')
+        --           local fullpath = vim.fs.normalize(file, ':p')
+        --           files[lang] = { ':' .. fullpath }
+        --         end
+        --
+        --         if files.default then
+        --           for lang, _ in pairs(files) do
+        --             if lang ~= 'default' then
+        --               vim.list_extend(files[lang], files.default)
+        --             end
+        --           end
+        --           files.default = nil
+        --         end
+        --         return files
+        --       end)(),
+        --     },
+        --   },
+        -- },
         -- tsserver = {},
         eslint_d = {},
         eslint = {},
