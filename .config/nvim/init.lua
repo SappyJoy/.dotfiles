4549f66d0dfb925f7b38ede8039b56043e66d268
Stepan Ponomarev <stponomarevw@gmail.com>
2025-04-10 14:00:22 +0300

    nvim: use icons if there are no Nerd font

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,21 +43,21 @@ require('lazy').setup(plugins, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
-    -- icons = vim.g.have_nerd_font and {} or {
-    --   cmd = '⌘',
-    --   config = '🛠',
-    --   event = '📅',
-    --   ft = '📂',
-    --   init = '⚙',
-    --   keys = '🗝',
-    --   plugin = '🔌',
-    --   runtime = '💻',
-    --   require = '🌙',
-    --   source = '📄',
-    --   start = '🚀',
-    --   task = '📌',
-    --   lazy = '💤 ',
-    -- },
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
   },
 })
 
