841f3dd8a992c0dd5262ab5996cf1560dfa16736
Stepan Ponomarev <stponomarevw@gmail.com>
2025-04-10 17:02:23 +0300

    nvim(base): refactor

--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -1,17 +1,24 @@
 return {
+  -- Undo Tree Visualization
   {
     'mbbill/undotree',
     keys = {
       {
         '<leader>u',
-        ':UndotreeToggle<CR>:UndotreeFocus<CR>',
-        desc = 'toggle undotree',
+        -- Chain commands: Toggle the undotree window AND focus it immediately
+        '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>',
+        desc = '[U]ndo Tree Toggle & Focus',
         silent = true,
+        noremap = true, -- Good practice for custom mappings
       },
     },
+    cmd = { 'UndotreeToggle', 'UndotreeFocus' }, -- Lazy load on command
   },
+
+  -- Seamless Navigation between Neovim and Tmux Panes
   {
     'christoomey/vim-tmux-navigator',
+    -- Lazy load when one of the navigation commands is invoked
     cmd = {
       'TmuxNavigateLeft',
       'TmuxNavigateDown',
@@ -19,14 +26,17 @@ return {
       'TmuxNavigateRight',
       'TmuxNavigatePrevious',
     },
+    -- Define keybindings for navigation
     keys = {
-      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+      { '<c-h>', '<Cmd><C-U>TmuxNavigateLeft<CR>', desc = 'Tmux Nav Left' },
+      { '<c-j>', '<Cmd><C-U>TmuxNavigateDown<CR>', desc = 'Tmux Nav Down' },
+      { '<c-k>', '<Cmd><C-U>TmuxNavigateUp<CR>', desc = 'Tmux Nav Up' },
+      { '<c-l>', '<Cmd><C-U>TmuxNavigateRight<CR>', desc = 'Tmux Nav Right' },
+      { '<c-\\>', '<Cmd><C-U>TmuxNavigatePrevious<CR>', desc = 'Tmux Nav Previous' },
     },
   },
+
+  -- Multiple Cursors / Selections Feature
   {
     'mg979/vim-visual-multi',
   },
