7a0371fba6e6bcae874b050233dc462305872753
Stepan Ponomarev <stponomarevw@gmail.com>
2025-04-11 10:28:54 +0300

    nvim(markdown): merge ftplugin and after/ftplugin

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,14 @@
+require("quarto").activate()
+
+--
+-- -- markdown link text objects for i and a
+-- vim.keymap.set({ "o", "x" }, "il", "<cmd>lua require('various-textobjs').mdlink('inner')<CR>", { buffer = true })
+-- vim.keymap.set({ "o", "x" }, "al", "<cmd>lua require('various-textobjs').mdlink('outer')<CR>", { buffer = true })
+vim.b.slime_cell_delimiter = '```'
+
+-- wrap text, but by word no character
+-- indent the wrappped line
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.wo.breakindent = true
 vim.wo.conceallevel = 2
