ebfe7b60e72dbfbcf284311b4dd4fcfc45a8ccf8
Stepan Ponomarev <stponomarevw@gmail.com>
2025-12-10 15:34:37 +0300

    nvim: Add nvim-aider

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -210,4 +210,70 @@ return {
     },
     build = 'make',
   },
+  {
+    'GeorgesAlkhouri/nvim-aider',
+    cmd = { 'Aider', 'AiderOpen' },
+    dependencies = {
+      { 'folke/snacks.nvim', version = '>=2.24.0' },
+      -- Optional integration: if you have neo-tree, this adds mappings
+      'nvim-neo-tree/neo-tree.nvim',
+    },
+    keys = {
+      { '<leader>a/', '<cmd>Aider toggle<cr>', desc = 'Toggle Aider' },
+      { '<leader>as', '<cmd>Aider send<cr>', desc = 'Send to Aider', mode = { 'n', 'v' } },
+      { '<leader>ac', '<cmd>Aider command<cr>', desc = 'Aider Commands' },
+      { '<leader>ab', '<cmd>Aider buffer<cr>', desc = 'Send Buffer' },
+      { '<leader>a+', '<cmd>Aider add<cr>', desc = 'Add File' },
+      { '<leader>a-', '<cmd>Aider drop<cr>', desc = 'Drop File' },
+      { '<leader>ar', '<cmd>Aider add readonly<cr>', desc = 'Add Read-Only' },
+      { '<leader>aR', '<cmd>Aider reset<cr>', desc = 'Reset Session' },
+    },
+    opts = {
+      aider_cmd = 'aider',
+      args = {
+        '--no-auto-commits',
+        '--pretty',
+      },
+      auto_reload = true,
+      theme = {
+        user_input_color = '#86B300',       -- ayu string
+        tool_output_color = '#399EE6',      -- ayu entity
+        tool_error_color = '#E65050',       -- ayu error
+        tool_warning_color = '#FA8D3E',     -- ayu warning
+        assistant_output_color = '#A37ACC', -- ayu constant
+        completion_menu_color = '#5C6166',  -- ayu fg
+        completion_menu_bg_color = '#F3F4F5', -- ayu panel_bg
+        completion_menu_current_color = '#5C6166', -- ayu fg
+        completion_menu_current_bg_color = '#D3E1F5', -- ayu selection_bg
+      },
+      config = {
+        os = { editPreset = 'nvim-remote' },
+        gui = { nerdFontsVersion = '3' },
+      },
+      win = {
+        wo = { winbar = 'Aider' },
+        style = 'nvim_aider',
+        position = 'right',
+      },
+    },
+    config = function(_, opts)
+      require('nvim_aider').setup(opts)
+
+      -- Neo-tree integration setup
+      -- Note: If you already configure neo-tree in another file, 
+      -- you may want to move this mapping logic there.
+      local has_neotree, _ = pcall(require, 'neo-tree')
+      if has_neotree then
+        require('nvim_aider.neo_tree').setup {
+          window = {
+            mappings = {
+              ['+'] = { 'nvim_aider_add', desc = 'add to aider' },
+              ['-'] = { 'nvim_aider_drop', desc = 'drop from aider' },
+              ['='] = { 'nvim_aider_add_read_only', desc = 'add read-only to aider' },
+            },
+          },
+        }
+      end
+    end,
+  },
 }
--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -1,44 +1,51 @@
 return {
-  -- Undo Tree Visualization
-  {
-    'mbbill/undotree',
-    keys = {
-      {
-        '<leader>u',
-        -- Chain commands: Toggle the undotree window AND focus it immediately
-        '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>',
-        desc = '[U]ndo Tree Toggle & Focus',
-        silent = true,
-        noremap = true, -- Good practice for custom mappings
-      },
+    -- Undo Tree Visualization
+    {
+        'mbbill/undotree',
+        keys = {
+            {
+                '<leader>u',
+                -- Chain commands: Toggle the undotree window AND focus it immediately
+                '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>',
+                desc = '[U]ndo Tree Toggle & Focus',
+                silent = true,
+                noremap = true, -- Good practice for custom mappings
+            },
+        },
+        cmd = { 'UndotreeToggle', 'UndotreeFocus' }, -- Lazy load on command
     },
-    cmd = { 'UndotreeToggle', 'UndotreeFocus' }, -- Lazy load on command
-  },
 
-  -- Seamless Navigation between Neovim and Tmux Panes
-  {
-    'christoomey/vim-tmux-navigator',
-    -- Lazy load when one of the navigation commands is invoked
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-      'TmuxNavigatorProcessList',
+    {
+        'christoomey/vim-tmux-navigator',
+        -- Lazy load when one of the navigation commands is invoked
+        cmd = {
+            'TmuxNavigateLeft',
+            'TmuxNavigateDown',
+            'TmuxNavigateUp',
+            'TmuxNavigateRight',
+            'TmuxNavigatePrevious',
+            'TmuxNavigatorProcessList',
+        },
+        -- Define keybindings for navigation
+        keys = {
+            { '<c-h>', '<Cmd><C-U>TmuxNavigateLeft<CR>', desc = 'Tmux Nav Left' },
+            { '<c-j>', '<Cmd><C-U>TmuxNavigateDown<CR>', desc = 'Tmux Nav Down' },
+            { '<c-k>', '<Cmd><C-U>TmuxNavigateUp<CR>', desc = 'Tmux Nav Up' },
+            { '<c-l>', '<Cmd><C-U>TmuxNavigateRight<CR>', desc = 'Tmux Nav Right' },
+            { '<c-\\>', '<Cmd><C-U>TmuxNavigatePrevious<CR>', desc = 'Tmux Nav Previous' },
+        },
+        config = function()
+            -- Add terminal-mode mappings to escape and navigate seamlessly
+            vim.keymap.set('t', '<C-h>', [[<C-\><C-n><Cmd>TmuxNavigateLeft<CR>]])
+            vim.keymap.set('t', '<C-j>', [[<C-\><C-n><Cmd>TmuxNavigateDown<CR>]])
+            vim.keymap.set('t', '<C-k>', [[<C-\><C-n><Cmd>TmuxNavigateUp<CR>]])
+            vim.keymap.set('t', '<C-l>', [[<C-\><C-n><Cmd>TmuxNavigateRight<CR>]])
+            vim.keymap.set('t', '<C-\\>', [[<C-\><C-n><Cmd>TmuxNavigatePrevious<CR>]])
+        end,
     },
-    -- Define keybindings for navigation
-    keys = {
-      { '<c-h>', '<Cmd><C-U>TmuxNavigateLeft<CR>', desc = 'Tmux Nav Left' },
-      { '<c-j>', '<Cmd><C-U>TmuxNavigateDown<CR>', desc = 'Tmux Nav Down' },
-      { '<c-k>', '<Cmd><C-U>TmuxNavigateUp<CR>', desc = 'Tmux Nav Up' },
-      { '<c-l>', '<Cmd><C-U>TmuxNavigateRight<CR>', desc = 'Tmux Nav Right' },
-      { '<c-\\>', '<Cmd><C-U>TmuxNavigatePrevious<CR>', desc = 'Tmux Nav Previous' },
-    },
-  },
 
-  -- Multiple Cursors / Selections Feature
-  {
-    'mg979/vim-visual-multi',
-  },
+    -- Multiple Cursors / Selections Feature
+    {
+        'mg979/vim-visual-multi',
+    },
 }
