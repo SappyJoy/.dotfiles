f2f265909926fe54eaf531c473e8e481b8b371f6
Stepan Ponomarev <stponomarevw@gmail.com>
2026-02-25 20:30:25 +0300

    layout: update keymaps for corne keyboard

--- .config/nvim/lua/plugins/editor/tmux-navigator.lua
+++ .config/nvim/lua/plugins/editor/tmux-navigator.lua
@@ -1,30 +1,31 @@
 return {
   {
     'christoomey/vim-tmux-navigator',
-    -- Lazy load when one of the navigation commands is invoked
     cmd = {
       'TmuxNavigateLeft',
       'TmuxNavigateDown',
       'TmuxNavigateUp',
       'TmuxNavigateRight',
       'TmuxNavigatePrevious',
-      'TmuxNavigatorProcessList',
     },
-    -- Define keybindings for navigation
     keys = {
-      { '<c-m>', '<Cmd><C-U>TmuxNavigateLeft<CR>', desc = 'Tmux Nav Left' },
-      { '<c-n>', '<Cmd><C-U>TmuxNavigateDown<CR>', desc = 'Tmux Nav Down' },
-      { '<c-e>', '<Cmd><C-U>TmuxNavigateUp<CR>', desc = 'Tmux Nav Up' },
-      { '<c-i>', '<Cmd><C-U>TmuxNavigateRight<CR>', desc = 'Tmux Nav Right' },
-      { '<c-\\>', '<Cmd><C-U>TmuxNavigatePrevious<CR>', desc = 'Tmux Nav Previous' },
+      { '<C-Left>',  '<cmd>TmuxNavigateLeft<cr>',  desc = 'Tmux Nav Left' },
+      { '<C-Down>',  '<cmd>TmuxNavigateDown<cr>',  desc = 'Tmux Nav Down' },
+      { '<C-Up>',    '<cmd>TmuxNavigateUp<cr>',    desc = 'Tmux Nav Up' },
+      { '<C-Right>', '<cmd>TmuxNavigateRight<cr>', desc = 'Tmux Nav Right' },
+      { '<C-\\>',    '<cmd>TmuxNavigatePrevious<cr>', desc = 'Tmux Nav Previous' },
     },
+    init = function()
+        -- Disable default mappings (C-h, C-j, etc) so only your C-Arrow keys work
+        vim.g.tmux_navigator_no_mappings = 1
+    end,
     config = function()
-      -- Add terminal-mode mappings to escape and navigate seamlessly
-      vim.keymap.set('t', '<C-h>', [[<C-\><C-n><Cmd>TmuxNavigateLeft<CR>]])
-      vim.keymap.set('t', '<C-j>', [[<C-\><C-n><Cmd>TmuxNavigateDown<CR>]])
-      vim.keymap.set('t', '<C-k>', [[<C-\><C-n><Cmd>TmuxNavigateUp<CR>]])
-      vim.keymap.set('t', '<C-l>', [[<C-\><C-n><Cmd>TmuxNavigateRight<CR>]])
-      vim.keymap.set('t', '<C-\\>', [[<C-\><C-n><Cmd>TmuxNavigatePrevious<CR>]])
+      -- Terminal mode (inside :term buffers)
+      vim.keymap.set('t', '<C-Left>',  [[<C-\><C-n><cmd>TmuxNavigateLeft<cr>]])
+      vim.keymap.set('t', '<C-Down>',  [[<C-\><C-n><cmd>TmuxNavigateDown<cr>]])
+      vim.keymap.set('t', '<C-Up>',    [[<C-\><C-n><cmd>TmuxNavigateUp<cr>]])
+      vim.keymap.set('t', '<C-Right>', [[<C-\><C-n><cmd>TmuxNavigateRight<cr>]])
+      vim.keymap.set('t', '<C-\\>',    [[<C-\><C-n><cmd>TmuxNavigatePrevious<cr>]])
     end,
   },
 }
