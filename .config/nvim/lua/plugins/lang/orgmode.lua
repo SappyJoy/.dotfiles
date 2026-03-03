decbbfa7ca10d18f2a43c4fba23029693f1df56b
Stepan Ponomarev <stponomarevw@gmail.com>
2026-03-04 01:15:03 +0300

    nvim: Enhance orgmode with telescope

--- .config/nvim/lua/plugins/lang/orgmode.lua
+++ .config/nvim/lua/plugins/lang/orgmode.lua
@@ -7,6 +7,12 @@ return {
       { 'nvim-treesitter/nvim-treesitter', lazy = true },
     },
     config = function()
+      local ts_ok, parsers = pcall(require, 'nvim-treesitter.parsers')
+      if ts_ok and not parsers.has_parser 'org' then
+        vim.cmd 'TSInstall org'
+      end
+
+      -- 2. Основной сетап
       require('orgmode').setup {
         org_agenda_files = { '~/orgfiles/**/*' },
         org_default_notes_file = '~/orgfiles/inbox.org',
@@ -27,9 +33,45 @@ return {
             target = '~/orgfiles/prompts.org',
           },
         },
+        mappings = {
+          org = {
+            org_refile = nil,
+          },
+        },
       }
 
-      -- Optional: Register Which-Key labels for the default Orgmode mappings
+      -- 3. Настраиваем "Obsidian-поведение" через Autocmd
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'org',
+        callback = function()
+          local opts = { noremap = true, silent = true, buffer = true }
+
+          -- Refile через Telescope (теперь точно сработает)
+          vim.keymap.set('n', '<leader>or', '<cmd>Telescope orgmode refile_heading<CR>', opts)
+
+          -- Enter в Normal Mode = переключить чекбокс (или выполнить действие под курсором)
+          vim.keymap.set('n', '<CR>', function()
+            require('orgmode').action 'org_mappings.org_ctrl_c_ctrl_c'
+          end, opts)
+
+          -- Enter в Insert Mode = умное создание нового пункта списка
+          vim.keymap.set('i', '<CR>', function()
+            local line = vim.api.nvim_get_current_line()
+            -- Если строка начинается с "- [ ] " или "- [x] "
+            if line:match '^%s*-%s*%[%s*[xX ]%s*%]' then
+              return '\n- [ ] '
+            -- Если строка начинается просто с "- " (список)
+            elseif line:match '^%s*-%s+' then
+              return '\n- '
+            -- Иначе обычный Enter
+            else
+              return '\n'
+            end
+          end, { expr = true, buffer = true })
+        end,
+      })
+
+      -- Which-Key лейблы
       local ok, wk = pcall(require, 'which-key')
       if ok then
         wk.add {
@@ -51,6 +93,7 @@ return {
             desc = 'Search content',
           },
           { '<leader>oh', '<cmd>Telescope orgmode search_headings<CR>', desc = 'Search headings' },
+          { '<leader>or', '<cmd>Telescope orgmode refile_heading<CR>', desc = 'Refile (Move)' },
         }
       end
     end,
