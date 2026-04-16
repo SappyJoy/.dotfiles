6c68faea0c1dbc414efc47ef7c947761bca6b1e4
Stepan Ponomarev <stponomarevw@gmail.com>
2026-04-16 12:21:17 +0300

    nvim: comment out uv python bin

--- .config/nvim/lua/sap/globals.lua
+++ .config/nvim/lua/sap/globals.lua
@@ -1,3 +1,11 @@
+-- -- 1. Construct the path to the UV-managed Python 3.12
+-- -- Based on your `uv python list` output:
+-- local uv_python_bin_path = vim.fn.expand("$HOME/.local/share/uv/python/cpython-3.12.9-linux-x86_64-gnu/bin")
+--
+-- -- 2. Prepend this path to the Neovim $PATH environment variable
+-- -- This forces Mason to use UV's Python 3.12 instead of system Python 3.8
+-- vim.env.PATH = uv_python_bin_path .. ":" .. vim.env.PATH
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
